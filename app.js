2bf9f5fe4aa96f0194180f68075573ff51e02212
aldiseptianaid05-lgtm <aldiseptiana.id05@gmail.com>
2026-07-10 10:22:02 +0800

    feat: implement localStorage as mock database helper

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
 console.log("Aplikasi Google Keep siap dijalankan!");
+let notes = JSON.parse(localStorage.getItem('google_keep_notes')) || [];
+function saveToMockDB() {
+    localStorage.setItem('google_keep_notes', JSON.stringify(notes));
+console.log("Data catatan saat ini di Mock DB:", notes);
