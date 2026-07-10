05505102936600a60a2e289ccd45a9a6527f835d
aldiseptianaid05-lgtm <aldiseptiana.id05@gmail.com>
2026-07-10 10:24:38 +0800

    feat: implement localStorage as mock database helper

--- app.js
+++ app.js
@@ -3,3 +3,4 @@ let notes = JSON.parse(localStorage.getItem('google_keep_notes')) || [];
 function saveToMockDB() {
     localStorage.setItem('google_keep_notes', JSON.stringify(notes));
 console.log("Data catatan saat ini di Mock DB:", notes);
+}
